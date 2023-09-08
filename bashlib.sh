bfe1021ced14c7b7407d03e8315db7139ba907b4
Joe K. Kim <joe.kim@intel.com>
2023-09-08 10:45:19 -0700

    Provide lock directory with specified folder or shared dir
    - Updated `bashlib.sh` and added `LOCKDIR` for `lock` func,
    where it will look for `BASHLIB_LOCKDIR` for input and fall back
    to `SHARED_DIR` if unspecified.

--- bashlib.sh
+++ bashlib.sh
@@ -39,7 +39,8 @@ if [[ -z $BASHLIB_DEFAULT_FUNCTIONS_SET ]]; then
         failignore
         if [[ -z "$1" ]]; then return 1; fi
         local HASH="$(echo "$1" | shasum -a 256)"; HASH="${HASH:0:32}"
-        local LOCKF="/tmp/$HASH.lock"
+        local LOCKDIR="$BASHLIB_LOCKDIR" ; if [[ -z "$LOCKDIR" ]]; then LOCKDIR="$SHARED_DIR"; fi
+        local LOCKF="$LOCKDIR/tmp/$HASH.lock"
         local EXP="$2"; [[ -z "$EXP" ]] && EXP=60; (( EXP=EXP+$(date +%s) ));
         local LOCKER=
         if ( set -o noclobber; echo "$$:$EXP" > "$LOCKF" ) 2> /dev/null; then
